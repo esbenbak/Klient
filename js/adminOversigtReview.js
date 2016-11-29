3500b41e6b2825d510e36ab5f5bb6a75ba134024
esbenbak <esben_bak@hotmail.com>
2016-11-29 19:02:08 +0100

    forsøger at fixe slet- og ratebutton

--- js/adminOversigtReview.js
+++ js/adminOversigtReview.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
               "<tr>" +
               "<td>" + decrypted.comment + "</td>" +
               "<td>" + decrypted.rating + "</td>" +
+              "<td class='btn-row'> <button class='btn btn-default toDelete' data-delete=>Slet upassende kommentar</button> </td>" +
               "</tr>"
           );
 
